2233be172fa5c20322faac7b3f2f698d3dccd130
мария арещенко <mariya.areschenko@gmail.com>
2022-04-28 23:39:17 +0300

    Добавьте файлы проекта.

---
+++ Explorer/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Explorer
+{
+    partial class FileExplorer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // backBtn
+            // 
+            this.backBtn.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.backBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.backBtn.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.backBtn.Location = new System.Drawing.Point(21, 12);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(50, 28);
+            this.backBtn.TabIndex = 0;
+            this.backBtn.Text = "<<";
+            this.backBtn.UseVisualStyleBackColor = false;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.nextBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.nextBtn.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nextBtn.Location = new System.Drawing.Point(77, 12);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(50, 28);
+            this.nextBtn.TabIndex = 1;
+            this.nextBtn.Text = ">>";
+            this.nextBtn.UseVisualStyleBackColor = false;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // openBtn
+            // 
+            this.openBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.openBtn.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.openBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.openBtn.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.openBtn.Location = new System.Drawing.Point(680, 12);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(108, 32);
+            this.openBtn.TabIndex = 2;
+            this.openBtn.Text = "Открыть";
+            this.openBtn.UseVisualStyleBackColor = false;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.Font = new System.Drawing.Font("Montserrat", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fileLabel.Location = new System.Drawing.Point(133, 14);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(51, 24);
+            this.fileLabel.TabIndex = 3;
+            this.fileLabel.Text = "Путь";
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileTextBox.Location = new System.Drawing.Point(198, 17);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.Size = new System.Drawing.Size(444, 22);
+            this.fileTextBox.TabIndex = 4;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(12, 50);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(776, 400);
+            this.webBrowser.TabIndex = 5;
+            // 
+            // FileExplorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.fileLabel);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.backBtn);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "FileExplorer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Проводник";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ Explorer/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Explorer
+{
+    public partial class FileExplorer : Form
+    {
+        public FileExplorer()
+        {
+            InitializeComponent();
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog browserDialog = new FolderBrowserDialog() { Description = "Выберите свой путь" })
+            {
+                if (browserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser.Url = new Uri(browserDialog.SelectedPath);
+                    fileTextBox.Text = browserDialog.SelectedPath;
+                }
+            }
+        }
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoForward)
+            {
+                webBrowser.GoForward();
+            }
+        }
+        private void backBtn_Click(object sender, EventArgs e)
+        {
+            if (webBrowser.CanGoBack)
+            {
+                webBrowser.GoBack();
+            }
+        }
+    }
+}
